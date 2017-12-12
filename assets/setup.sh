65928867545c29ff9c85e3f0544def4e1022f50e
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-12-12 23:27:38 +0900

    fix: コメント追加

--- assets/setup.sh
+++ assets/setup.sh
@@ -1,15 +1,20 @@
-#!bin/bash
+#!/bin/bash
 
-yum -q -y install openssh-server openssh-client &&
-
-ssh-keygen -t rsa1    -N '' -f /etc/ssh/ssh_host_key > /dev/null &&
-ssh-keygen -t rsa     -N '' -f /etc/ssh/ssh_host_rsa_key > /dev/null &&
-ssh-keygen -t dsa     -N '' -f /etc/ssh/ssh_host_dsa_key > /dev/null &&
-ssh-keygen -t ecdsa   -N '' -f /etc/ssh/ssh_host_ecdsa_key > /dev/null &&
-
+# Install OpenSSH
+yum -q -y update &&
+yum -q -y install openssh-server openssh-clients &&
 mkdir /var/run/sshd &&
+
+# Generate host keys
+ssh-keygen -t rsa1  -N '' -f /etc/ssh/ssh_host_key > /dev/null &&
+ssh-keygen -t rsa   -N '' -f /etc/ssh/ssh_host_rsa_key > /dev/null &&
+ssh-keygen -t dsa   -N '' -f /etc/ssh/ssh_host_dsa_key > /dev/null &&
+ssh-keygen -t ecdsa -N '' -f /etc/ssh/ssh_host_ecdsa_key > /dev/null &&
+
+# Setup user
 echo 'root:admin' | chpasswd &&
 
+# Remove installation files
 rm -r /assets/
 
 exit $?
