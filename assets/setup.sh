2d1f66b4f8e23524ad8cfca25d94bb53ade73b80
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-02-25 14:23:38 +0900

    fix: trivial

--- assets/setup.sh
+++ assets/setup.sh
@@ -3,6 +3,14 @@
 # Change root password
 echo 'root:admin' | chpasswd &&
 
+# Add group & user
+useradd -u 1000 -m setup &&
+echo 'setup:manager' | chpasswd &&
+
+# Generate locale
+localedef -i ja_JP -f UTF-8     -c -A /usr/share/locale/locale.alias ja_JP.UTF-8 &&
+localedef -i ja_JP -f SHIFT_JIS -c -A /usr/share/locale/locale.alias ja_JP.SHIFT_JIS &&
+
 # Install OpenSSH
 yum -q -y install openssh-server openssh-clients &&
 mkdir /var/run/sshd &&
@@ -12,14 +20,6 @@ ssh-keygen -t rsa1  -N '' -f /etc/ssh/ssh_host_key > /dev/null &&
 ssh-keygen -t rsa   -N '' -f /etc/ssh/ssh_host_rsa_key > /dev/null &&
 ssh-keygen -t dsa   -N '' -f /etc/ssh/ssh_host_dsa_key > /dev/null &&
 ssh-keygen -t ecdsa -N '' -f /etc/ssh/ssh_host_ecdsa_key > /dev/null &&
-
-# Generate locale
-localedef -i ja_JP -f UTF-8     -c -A /usr/share/locale/locale.alias ja_JP.UTF-8 &&
-localedef -i ja_JP -f SHIFT_JIS -c -A /usr/share/locale/locale.alias ja_JP.SHIFT_JIS &&
-
-# Add group & user
-useradd -u 1000 -m setup &&
-echo 'setup:manager' | chpasswd &&
 
 # Remove installation files
 rm -r /assets/
